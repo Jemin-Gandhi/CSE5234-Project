0539ef125930a0653c86d807b0ff26344984b16b
Jemin Gandhi <jemingandhi16@gmail.com>
2025-10-07 18:26:18 -0400

    not working purchase page

---
+++ website/src/components/purchase.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Purchase = () => {
+  let title = "Purchase Page";
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      Hello!
+    </div>
+  );
+};
+
+export default Purchase;
