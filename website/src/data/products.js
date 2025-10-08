1a8306588d8bf058967bd279da38b344089bd2fe
Gaige McMichael <gaigemcmichael@gmail.com>
2025-10-08 16:19:20 -0400

    purchase page

---
+++ website/src/data/products.js
@@ -0,0 +1,39 @@
+const PRODUCTS = [
+  { 
+    id: 1, 
+    name: 'Car 1', 
+    price: 9.99, 
+    img: '/images/corvette.jpg',
+    description: 'Description for Car 1' 
+  },
+  { 
+    id: 2, 
+    name: 'Car 2', 
+    price: 12.49, 
+    img: '/images/corvette.jpg',
+    description: 'Description for Car 2' 
+  },
+  { 
+    id: 3, 
+    name: 'Car 3', 
+    price: 7.99, 
+    img: '/images/corvette.jpg',
+    description: 'Description for Car 3' 
+  },
+  { 
+    id: 4, 
+    name: 'Car 4', 
+    price: 14.99, 
+    img: '/images/corvette.jpg',
+    description: 'Description for Car 4' 
+  },
+  { 
+    id: 5, 
+    name: 'Car 5', 
+    price: 4.99, 
+    img: '/images/corvette.jpg',
+    description: 'Description for Car 5' 
+  },
+];
+
+export default PRODUCTS;
